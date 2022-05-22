1c3a557e276890d72949660285f4864e59bc5e03
Rui Kang <1539678784@qq.com>
2022-05-22 22:48:02 +0800

    init

---
+++ src/main/java/cn/edu/thssdb/type/ConditionType.java
@@ -0,0 +1,5 @@
+package cn.edu.thssdb.type;
+
+public enum ConditionType {
+    EQ, NE, GT, LT, GE, LE
+}
---
+++ src/main/java/cn/edu/thssdb/type/ExpressionType.java
@@ -0,0 +1,5 @@
+package cn.edu.thssdb.type;
+
+public enum ExpressionType {
+    NUMBER, STRING, COLUMN, NULL
+}
